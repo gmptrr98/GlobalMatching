0296cfe68fe0a354e03f683bd3b368283566efb3
Giampietro Nigro <110682193+gmptrr98@users.noreply.github.com>
2024-02-26 09:36:55 +0100

    Add files via upload

--- GlobalMatching.py
+++ GlobalMatching.py
@@ -6,7 +6,26 @@ def GlobalMatching(frame: cv2.typing.MatLike,
                    template: cv2.typing.MatLike, 
                    template_name: str, 
                    search_area: List[Tuple[int, int]],
-                   mask: cv2.typing.MatLike = None) -> Tuple[bool,List[int], str, float]:
+                   mask: cv2.typing.MatLike = None) -> Tuple[bool,List[int], str, float]: 
+    
+    """_summary_
+
+    Args:
+        frame (cv2.typing.MatLike): Matlike image.
+        template (cv2.typing.MatLike): Matlike image.
+        template_name (str): Name of template search request, used just to identify return value.
+        search_area (List[Tuple[int, int]]): (Top Left Corner [x1 y1], Bottom Rigth Corner [x2 y2]).
+        mask (cv2.typing.MatLike, optional): Matlike image used as a mask on template. Defaults to None.
+
+    Raises:
+        ValueError incorrect mask type: Mask is not passed as a Matlike.
+        ValueError incorrect mask shape: Mask shape is not the same as template. 
+        ValueError incoherent search area: Search area is smaller than template.
+
+    Returns:
+        Tuple[bool,List[int], str, float]: (Presence of template in frame, Bottom left corner of template in frame [x y], Template name, Confidence level)
+    """    
+    
     if mask is not None:
         if not isinstance(mask,cv2.typing.MatLike):
             raise ValueError ("Mask type incorrect")
@@ -14,9 +33,10 @@ def GlobalMatching(frame: cv2.typing.MatLike,
             raise ValueError ("Mask size do not match template size")
         mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     try:
+
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-        threshold = 0.95
+        threshold = 0.9
 
         cutted_frame = None
 
@@ -36,9 +56,9 @@ def GlobalMatching(frame: cv2.typing.MatLike,
         if max_val>threshold:
             bottom_left = max_loc
             if cutted_frame.any():
-                bottom_left = [bottom_left[0] + top_left_search[0], bottom_left[1] + bottom_right_search[1]]
+                bottom_left = [bottom_left[0] + top_left_search[0], bottom_left[1] + top_left_search[1]]
             
-            return (True, max_loc, template_name, max_val)
+            return (True, bottom_left, template_name, max_val)
 
         else:
             return (False, [0, 0], template_name, max_val)
